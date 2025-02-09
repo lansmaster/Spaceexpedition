eb5b50d9b760fbb793035a35a57c6a7ca8463964
lansmaster <stepanbaryhev@gmail.com>
2025-02-09 21:17:50 +0300

    movement+rotation (beta)

---
+++ Assets/_Project/Scripts/CameraFollowSystem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraFollowSystem : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _distance = 15f;
+    [SerializeField] private float _height = 5f;
+    [SerializeField] private float _positionSmoothTime = 0.2f;
+    [SerializeField] private float _rotationSmoothTime = 0.1f;
+
+    private Vector3 _offset;
+    private Vector3 _currentVelocity;
+    private float _rotationVelocity;
+
+    private void Start()
+    {
+        _offset = new Vector3(0, _height, -_distance);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = _target.position + _target.TransformDirection(_offset);
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref _currentVelocity, _positionSmoothTime);
+
+        Quaternion desiredRotation = Quaternion.LookRotation(_target.position - transform.position, _target.up);
+
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, desiredRotation.eulerAngles.y, ref _rotationVelocity, _rotationSmoothTime),
+            transform.rotation.eulerAngles.z
+        );
+    }
+}
